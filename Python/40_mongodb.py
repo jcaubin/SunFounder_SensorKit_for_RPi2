3ac8f1fcec3f067363dcf6b215bae86829f3403e
Julio Caubín <jcaubin@gmail.com>
2019-12-26 09:01:06 +0100

    Update 40_mongodb.py
    uri modificada

--- Python/40_mongodb.py
+++ Python/40_mongodb.py
@@ -2,7 +2,7 @@
 
 import pymongo as pm
 
-uri = "mongodb://2b41d18f-0ee0-4-231-b9ee:mNlRbGPqQMAbtmEhzL7F8JR0cv52nnYTZRW5qWVpV8VCxW6YFuwHNUPkI9BwFX8Bh9Opx9qFVxePZLJ39qjscQ==@2b41d18f-0ee0-4-231-b9ee.documents.azure.com:10255/?ssl=true&replicaSet=globaldb"
+uri = ""
 myclient = pm.MongoClient(uri)
 
 mydb=myclient["demoDb"]
